e362d16ab9d62de47b1c8e67f7dcc662cca213e2
Xavierrodgriues <xavierrodgriues123@gmail.com>
2025-10-16 01:59:31 +0530

    fix: countdown to 18th and cleaned about&contact

--- vendor/countdowntime/countdowntime.js
+++ vendor/countdowntime/countdowntime.js
@@ -1,50 +1,53 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    function getTimeRemaining(endtime) { 
-      var t = Date.parse(endtime) - Date.parse(new Date());
-      var seconds = Math.floor((t / 1000) % 60);
-      var minutes = Math.floor((t / 1000 / 60) % 60);
-      var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-      var days = Math.floor(t / (1000 * 60 * 60 * 24));
-      return {
-        'total': t,
-        'days': days,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
-      };
-    }
+  function getTimeRemaining(endtime) { 
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
 
-    function initializeClock(endtime) { 
-      var daysSpan = $('.days');
-      var hoursSpan = $('.hours');
-      var minutesSpan = $('.minutes');
-      var secondsSpan = $('.seconds');
+  function initializeClock(endtime) { 
+    var daysSpan = $('.days');
+    var hoursSpan = $('.hours');
+    var minutesSpan = $('.minutes');
+    var secondsSpan = $('.seconds');
 
-      function updateClock() { 
-        var t = getTimeRemaining(endtime);
+    function updateClock() { 
+      var t = getTimeRemaining(endtime);
 
-        daysSpan.html(t.days);
-        hoursSpan.html(('0' + t.hours).slice(-2));
-        minutesSpan.html(('0' + t.minutes).slice(-2));
-        secondsSpan.html(('0' + t.seconds).slice(-2));
+      daysSpan.html(t.days);
+      hoursSpan.html(('0' + t.hours).slice(-2));
+      minutesSpan.html(('0' + t.minutes).slice(-2));
+      secondsSpan.html(('0' + t.seconds).slice(-2));
 
-        if (t.total <= 0) {
-          clearInterval(timeinterval);
-        }
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
       }
-
-      updateClock();
-      var timeinterval = setInterval(updateClock, 1000);
     }
 
-    // Get next Friday
-    var now = new Date();
-    var nextFriday = new Date();
-    nextFriday.setDate(now.getDate() + ((5 - now.getDay() + 7) % 7)); // 5 = Friday
-    nextFriday.setHours(0, 0, 0, 0); // Set to midnight of Friday
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
 
-    initializeClock(nextFriday);
+  // Countdown to Saturday, 18th of this month
+  var now = new Date();
+  var targetDate = new Date(now.getFullYear(), now.getMonth(), 18, 0, 0, 0); // 18th at midnight
+
+  // If the 18th has already passed, set it for next month
+  if (now > targetDate) {
+    targetDate = new Date(now.getFullYear(), now.getMonth() + 1, 18, 0, 0, 0);
+  }
+
+  initializeClock(targetDate);
 
 })(jQuery);
